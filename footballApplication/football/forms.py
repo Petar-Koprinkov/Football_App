e409d10553b3488dd6a523323c3a898065d4c3ad
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-10-06 12:50:06 +0300

    feat: Added validation, widgets and methods

--- footballApplication/football/forms.py
+++ footballApplication/football/forms.py
@@ -8,3 +8,30 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('author', 'content')
 
+        labels = {
+            'author': '',
+            'content': '',
+        }
+
+        widgets = {
+            'author': forms.TextInput(attrs={'placeholder': 'Please enter your name'}),
+            'content': forms.Textarea(attrs={'placeholder': 'Please enter your comment'}),
+        }
+
+        error_messages = {
+            'author': {
+                'max_length': 'You have entered too many characters.',
+            }
+        }
+
+    def save(self, commit=True):
+        comment = super().save(commit=False)
+        comment.author = comment.author.capitalize()
+
+        if commit:
+            comment.save()
+
+        return comment
+
+
+
